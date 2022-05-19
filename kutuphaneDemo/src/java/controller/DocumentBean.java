60d104d3d29aee63b2b2e732c345fe9c635e3f98
Aybüke Gürer <ce.aybukegurer@gmail.com>
2022-05-19 23:12:22 +0300

    dosyalama için; entity, dao, bean ve xhtml sınıfları oluşturuldu. İçerikleri yazıldı.

---
+++ kutuphaneDemo/src/java/controller/DocumentBean.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dao.DocumentDAO;
+import entity.Document;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import jakarta.servlet.http.Part;
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+/**
+ *
+ * @author 90553
+ */
+@Named(value = "documentController")
+@SessionScoped
+public class DocumentBean implements Serializable{
+    
+    private Document document;
+    private List<Document> documentList;
+    private DocumentDAO documentDao;
+    
+    private Part doc;
+    
+    private String uploadTo = "/Users/90553/Desktop/upload/";
+    
+    public void upload(){
+        try {
+            InputStream input = doc.getInputStream();
+            File f = new File(uploadTo+doc.getSubmittedFileName());
+            Files.copy(input, f.toPath());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getUploadTo() {
+        return uploadTo;
+    }
+
+    public Document getDocument() {
+        if(this.document == null){
+            this.document = new Document();
+        }
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    public List<Document> getDocumentList() {
+        this.documentList  = this.getDocumentDao().findAll(); 
+        return documentList;
+    }
+
+    public void setDocumentList(List<Document> documentList) {
+        this.documentList = documentList;
+    }
+
+    public DocumentDAO getDocumentDao() {
+        if(this.documentDao == null){
+            this.documentDao = new DocumentDAO ();
+        }
+        return documentDao;
+    }
+
+    public void setDocumentDao(DocumentDAO documentDao) {
+        this.documentDao = documentDao;
+    }
+
+    public Part getDoc() {
+        return doc;
+    }
+
+    public void setDoc(Part doc) {
+        this.doc = doc;
+    }
+    
+    
+    
+}
---
+++ kutuphaneDemo/src/java/entity/Document.java
@@ -0,0 +1,42 @@
+  /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author 90553
+ */
+public class Document {
+    
+    private String dosyaYolu;
+    private String dosyaAdi;
+    private String dosyaTipi;
+
+    public String getDosyaYolu() {
+        return dosyaYolu;
+    }
+
+    public void setDosyaYolu(String dosyaYolu) {
+        this.dosyaYolu = dosyaYolu;
+    }
+
+    public String getDosyaAdi() {
+        return dosyaAdi;
+    }
+
+    public void setDosyaAdi(String dosyaAdi) {
+        this.dosyaAdi = dosyaAdi;
+    }
+
+    public String getDosyaTipi() {
+        return dosyaTipi;
+    }
+
+    public void setDosyaTipi(String dosyaTipi) {
+        this.dosyaTipi = dosyaTipi;
+    }
+    
+    
+}
